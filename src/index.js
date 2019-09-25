0ff8f46f3d387b8b3fc6c0775a0a44f9a6403479
harrylincoln <harrylincoln0612@gmail.com>
2019-09-25 16:23:06 +0100

    incorrect deps

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
 import createReactiveClass from './createReactiveClass';
+import DOM from 'react-dom-factories';
 
 export function reactive(reactClass) {
   return createReactiveClass(reactClass);
 }
 
-export const dom = Object.keys(React.DOM).reduce((result, tag) => {
+export const dom = Object.keys(DOM).reduce((result, tag) => {
   result[tag] = createReactiveClass(tag);
   return result;
 }, {});
