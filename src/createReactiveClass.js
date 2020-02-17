05482e9ecc5e13242f8b034ef72f0d90c75cf4b3
Isma Perez <pb.ismael@gmail.com>
2020-02-17 16:48:47 +0000

    forwardRef

--- src/createReactiveClass.js
+++ src/createReactiveClass.js
@@ -3,7 +3,7 @@ import useSubscription from './useSubscription';
 import mapValues from 'lodash/fp/mapValues';
 import omit from 'lodash/fp/omit';
 
-export default component => props => {
+export default component => React.forwardRef((props, ref) => {
   const usePropSubscription = prop$ => {
     const [prop, setProp] = React.useState();
 
@@ -12,11 +12,14 @@ export default component => props => {
     return prop;
   };
 
-  const newProps = mapValues(prop => (prop.subscribe ? usePropSubscription(prop) : prop), props);
+  const newProps = {
+    ...mapValues(prop => (prop.subscribe ? usePropSubscription(prop) : prop), props),
+    ref
+  };
 
   return React.createElement(
     React.Fragment,
     {},
     props.mount && !newProps.mount ? null : React.createElement(component, omit('mount', newProps))
   );
-};
+});
