eac2ee804c4e5853e87c433c6a0683fc73d7d646
David Garner <dgarner@qmetric.co.uk>
2021-05-19 11:51:58 +0100

    Fix linting errors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import createReactiveClass from './createReactiveClass';
 import DOM from 'react-dom-factories';
+import createReactiveClass from './createReactiveClass';
 
 export function reactive(reactClass) {
   return createReactiveClass(reactClass);
 }
 
 export const dom = Object.keys(DOM).reduce((result, tag) => {
+  // eslint-disable-next-line no-param-reassign
   result[tag] = createReactiveClass(tag);
   return result;
 }, {});
